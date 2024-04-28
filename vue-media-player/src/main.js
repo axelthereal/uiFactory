5c0be423a996537babf7249ed86cb21f963f3709
axelthereal <mail.axeltokam@gmail.com>
2024-04-28 02:30:00 +0100

    added icons package

--- vue-media-player/src/main.js
+++ vue-media-player/src/main.js
@@ -1,4 +1,7 @@
-import "./assets/main.css";
+import "./assets/css/main.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
+import "./assets/bootstrap/icons/bootstrap-icons.css";
 
 import { createApp } from "vue";
 import App from "./App.vue";
