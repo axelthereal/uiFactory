248c6f68e172fc422ac8cdb4def41e70393d6c50
axelthereal <mail.axeltokam@gmail.com>
2024-04-28 05:42:45 +0100

    edit and make minor adjustments to project files

--- vue-media-player/src/App.vue
+++ vue-media-player/src/App.vue
@@ -1,7 +1,14 @@
 <template>
   <header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-transparent fixed-top" aria-label="Tenth navbar example">
+      <div class="container-fluid justify-content-center">
+        <a href="" class="navbar-brand text-center fs-4 fw-semibold d-flex align-items-center gap-2">
+          <img src="/cdn/images/favicon.ico" alt="">
+          VuePlayer</a>
+      </div>
+    </nav>
   </header>
-  <main class="container col-11 col-lg-9 body-center">
+  <main class="container col-11 col-lg-9">
     <RouterView />
   </main>
 </template>
@@ -14,4 +21,12 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.navbar {
+  background-color: var(--body-background) !important;
+}
+
+main {
+  margin-top: 150px;
+}
+</style>
--- vue-media-player/src/views/HomeView.vue
+++ vue-media-player/src/views/HomeView.vue
@@ -1,9 +1,19 @@
 <template>
   <main>
-    <h1 class="fs-large text-center mx-auto w-80">Listen to<br>your
+    <h1 class="fs-1 fw-large text-center mx-auto w-80">Listen to<br>your
       favorite
       songs.
     </h1>
+
+    <p class="text-center mt-4 fs-4 fw-medium">Stream all your songs and audio files in one place, and experience new
+      emotions.
+    </p>
+
+    <div class="songs-listcard">
+      <div class="music-card">
+        <img src="/cdn/images/music-card-01.png" class="song-cover" alt="">
+      </div>
+    </div>
   </main>
 </template>
 
